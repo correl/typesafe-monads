c61baf51c19097b4ddda23c2aaac519290709c4f
Samuele Reghenzi <sam@reghenzi.cloud>
2020-12-16 14:21:15 +0100

    set with test

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,8 +1,8 @@
 import pytest  # type: ignore
 from typing import Type
-from monads import Maybe, List, Result
+from monads import Maybe, List, Result, Set
 
 
-@pytest.fixture(scope="module", params=[Maybe, List, Result])
+@pytest.fixture(scope="module", params=[Maybe, List, Result, Set])
 def monad(request) -> Type:
     return request.param
